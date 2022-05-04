7c6378e81f70cf2bbcb2648f0d119fd7a1425ae9
Daniel Yang <danyang0724@gmail.com>
2022-05-03 20:44:32 -0400

    created Konamicode to enter website

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content was added by Javascript";
-document.querySelector("body").appendChild(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content was added by Javascript";
+// document.querySelector("body").appendChild(h2);
+const konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a", "Enter"]
+let userArray = []
+const loaded = false
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+    if (event.key === konamiCode[userArray.length]) {
+        userArray.push(event.key)
+    } else {
+        userArray = []
+        console.log("Wrong input try again!")
+    }
+
+    if (userArray.length === konamiCode.length && userArray.every((value, index) => value === konamiCode[index])) {
+        console.log("finish")
+        document.body.innerHTML = "<h1>This is the real website</h1>"
+    }
+});
