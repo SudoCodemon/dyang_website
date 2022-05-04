69d7358cc83e3536638c42995006f61e21fab6c6
Daniel Yang <danyang0724@gmail.com>
2022-05-04 13:51:21 -0400

    completed functional Konami code to show hidden website

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// const h2 = document.createElement("h2");
-// h2.textContent = "This content was added by Javascript";
-// document.querySelector("body").appendChild(h2);
-const konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a", "Enter"]
-let userArray = []
-const loaded = false
+const main = document.getElementById("main");
+const second = document.getElementById("second");
+const konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a", "Enter"];
+let userArray = [];
 
 document.addEventListener("keydown", function (event) {
     console.log(event);
@@ -16,6 +14,7 @@ document.addEventListener("keydown", function (event) {
 
     if (userArray.length === konamiCode.length && userArray.every((value, index) => value === konamiCode[index])) {
         console.log("finish")
-        document.body.innerHTML = "<h1>This is the real website</h1>"
+        main.style.display="none"
+        second.style.visibility="visible"
     }
 });
